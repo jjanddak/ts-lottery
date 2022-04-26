62944ea04a20741374eac1fd023783199ce5be76
jonghyeok <opplane@gractor.com>
2022-04-26 09:35:10 +0900

    추첨클릭시 추첨 완료까지 버튼 비활성화

--- src/App.tsx
+++ src/App.tsx
@@ -1,77 +1,90 @@
-import './App.css';
+import "./App.css";
 
 function App() {
-  function createLotto(){
-    let result:number[]=[];
+  function createLotto() {
+    // const btn: HTMLElement | null = document.getElementById('btn')
+    // btn?.setAttribute('disabled', '')
+    disableBtn(true)
+    let result: number[] = [];
 
-    function pickSome(digit:number){
-      let repeat = (Math.floor(Math.random()*10)%2)+1
+    function pickSome(digit: number) {
+      let repeat = (Math.floor(Math.random() * 10) % 2) + 1;
 
-      if(digit===40){
-        for(let i=0; i<repeat; i++){
-          if(result.length===6) return;
-          let pickedNum = (Math.floor(Math.random()*10)%6)+digit;
-          if(!result.includes(pickedNum)){
+      if (digit === 40) {
+        for (let i = 0; i < repeat; i++) {
+          if (result.length === 6) return;
+          let pickedNum = (Math.floor(Math.random() * 10) % 6) + digit;
+          if (!result.includes(pickedNum)) {
             result.push(pickedNum);
-          }else{
+          } else {
             i--;
           }
         }
-      }else if(digit===0){
-        for(let i=0; i<repeat; i++){
-          if(result.length===6) return;
-          let pickedNum = (Math.floor(Math.random()*10)%10)+1;
-          if(!result.includes(pickedNum)){
+      } else if (digit === 0) {
+        for (let i = 0; i < repeat; i++) {
+          if (result.length === 6) return;
+          let pickedNum = (Math.floor(Math.random() * 10) % 10) + 1;
+          if (!result.includes(pickedNum)) {
             result.push(pickedNum);
-          }else{
+          } else {
             i--;
           }
         }
-      }else{
-        for(let i=0; i<repeat; i++){
-          if(result.length===6) return;
-          let pickedNum = (Math.floor(Math.random()*10)%10)+digit;
-          if(!result.includes(pickedNum)){
+      } else {
+        for (let i = 0; i < repeat; i++) {
+          if (result.length === 6) return;
+          let pickedNum = (Math.floor(Math.random() * 10) % 10) + digit;
+          if (!result.includes(pickedNum)) {
             result.push(pickedNum);
-          }else{
+          } else {
             i--;
           }
         }
       }
     }
 
-    let digits:any[]=[0,10,20,30,40];
-    while(result.length<6){
-      let ran:number=(Math.floor(Math.random()*10)%5);
-      if(digits[ran]!==undefined){
+    let digits: any[] = [0, 10, 20, 30, 40];
+    for (let i = result.length; result.length < 6; i++) {
+      let ran: number = Math.floor(Math.random() * 10) % 5;
+      if (digits[ran] !== undefined) {
         let digit = digits[ran];
         pickSome(digit);
+        if (i === result.length) i--;
         digits[ran] = undefined;
       }
     }
-    
-    let bonus=(Math.floor(Math.random()*100)%45)+1
-    while(result.includes(bonus)){
-      bonus=(Math.floor(Math.random()*100)%45)+1;
+
+    let bonus = (Math.floor(Math.random() * 100) % 45) + 1;
+    while (result.includes(bonus)) {
+      bonus = (Math.floor(Math.random() * 100) % 45) + 1;
     }
 
-    result.sort((a,b)=>a-b);
-    let li:HTMLElement=document.createElement("li");
-    li.textContent=`${result.toString()} + ${bonus}`;
-    let div:HTMLElement|null=document.querySelector(".container");
-    div!.appendChild(li);
+    result.sort((a, b) => a - b);
+    let li: HTMLElement = document.createElement("li");
+    li.textContent = `${result.toString()} + ${bonus}`;
+    let div: HTMLElement | null = document.querySelector(".container");
+    div?.appendChild(li);
+    disableBtn(false)
+    // btn?.removeAttribute('disabled')
+  }
+
+  const disableBtn = (status: Boolean) => {
+    const btn = document.getElementById('btn')
+    if (status) btn?.setAttribute('disabled', 'disabled')
+    else btn?.removeAttribute('disabled')
   }
 
   return (
-    <div className="App">
+    <div className="App" >
       <header className="App-header">
-        <button onClick={createLotto}>
-          추첨하기!!
+        <button
+          id="btn"
+          onClick={createLotto}
+        >
+          !!
         </button>
 
-        <div className="container">
-
-        </div>
+        <div className="container"></div>
       </header>
     </div>
   );
