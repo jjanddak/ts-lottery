421e1996d04f532ffce70b91ba92667e066ace23
opplane <opplane@gmail.com>
2022-11-19 22:19:38 +0900

    연금복권 추가

--- src/App.tsx
+++ src/App.tsx
@@ -36,21 +36,29 @@ function App() {
     }
 
     function getYearLottery() {
-
+      const n = Math.floor(Math.random() * 1000000)
+      const round = Math.floor(Math.random() * 10 % 5) + 1
+      return `${round}조 ${n}`
     }
 
     const paper = getPaper()
     const year = getYearLottery()
 
     const container: HTMLElement | null = document.querySelector(".container");
+    const yearContainer: HTMLElement | null = document.querySelector(".yearContainer");
     const div: HTMLElement = document.createElement("div")
+    const yearLi: HTMLElement = document.createElement("li")
+    yearLi.textContent = year
+
     for (let i = 0; i < paper.length; i++) {
       let li: HTMLElement = document.createElement("li");
       li.textContent = `${paper[i].toString()}`;
       div?.appendChild(li);
     }
     if (container?.children) container.textContent = ''
+    if (yearContainer?.children) yearContainer.textContent = ''
     container?.appendChild(div)
+    yearContainer?.appendChild(yearLi)
   }
 
   return (
@@ -60,11 +68,13 @@ function App() {
           id="btn"
           onClick={createLotto}
         >
-          추첨!!
+          Pick Your Number
         </button>
 
+        <div><h4>로또</h4></div>
         <div className="container"></div>
-        <div className="yearContainer" >hello world</div>
+        <div><h4>연금복권</h4></div>
+        <div className="yearContainer" ></div>
       </header >
     </div >
   );
