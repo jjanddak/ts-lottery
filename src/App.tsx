d165100eac9ec8f3467f46c5c7c03ace4e8b4a7e
opplane <opplane@gmail.com>
2022-11-19 21:59:12 +0900

    getLotto improve

--- src/App.tsx
+++ src/App.tsx
@@ -2,76 +2,55 @@ import "./App.css";
 
 function App() {
   function createLotto() {
-    // const btn: HTMLElement | null = document.getElementById('btn')
-    // btn?.setAttribute('disabled', '')
-    disableBtn(true)
-    let result: number[] = [];
+    function getPaper() {
 
-    function pickSome(digit: number) {
-      let repeat = (Math.floor(Math.random() * 10) % 2) + 1;
+      const round = 5
+      const paper: any = []
+      for (let i = 0; i < round; i++) {
+        const getRan = (min: number, max: number) => {
+          min = min === 0 ? 0 : 1
+          return (Math.floor(Math.random() * 10) % (max + 1 - min)) + min
+        }
+        const lottoNums: any = []
+        while (lottoNums.length < 6) {
+          let digit = getRan(0, 4)
+          digit = digit === 0 ? 0 : digit * 10
+          let duplicate = getRan(1, 2) // max <= 6개까지 남은 개수
+          let round = 1
+          while (round <= duplicate) {
 
-      if (digit === 40) {
-        for (let i = 0; i < repeat; i++) {
-          if (result.length === 6) return;
-          let pickedNum = (Math.floor(Math.random() * 10) % 6) + digit;
-          if (!result.includes(pickedNum)) {
-            result.push(pickedNum);
-          } else {
-            i--;
+
+            const n = digit === 40 ? getRan(0, 5) : getRan(0, 9)
+            const num = digit + n
+            if (!lottoNums.includes(num)) {
+              lottoNums.push(num === 0 ? 1 : num)
+              if (lottoNums.length + duplicate > 6) duplicate = 6 - lottoNums.length
+              round++
+            }
           }
         }
-      } else if (digit === 0) {
-        for (let i = 0; i < repeat; i++) {
-          if (result.length === 6) return;
-          let pickedNum = (Math.floor(Math.random() * 10) % 10) + 1;
-          if (!result.includes(pickedNum)) {
-            result.push(pickedNum);
-          } else {
-            i--;
-          }
-        }
-      } else {
-        for (let i = 0; i < repeat; i++) {
-          if (result.length === 6) return;
-          let pickedNum = (Math.floor(Math.random() * 10) % 10) + digit;
-          if (!result.includes(pickedNum)) {
-            result.push(pickedNum);
-          } else {
-            i--;
-          }
-        }
+        lottoNums.sort((a: number, b: number) => a - b)
+        paper.push(lottoNums)
       }
+      return paper
     }
 
-    let digits: any[] = [0, 10, 20, 30, 40];
-    for (let i = result.length; result.length < 6; i++) {
-      let ran: number = Math.floor(Math.random() * 10) % 5;
-      if (digits[ran] !== undefined) {
-        let digit = digits[ran];
-        pickSome(digit);
-        if (i === result.length) i--;
-        digits[ran] = undefined;
-      }
+    function getYearLottery() {
+
     }
 
-    let bonus = (Math.floor(Math.random() * 100) % 45) + 1;
-    while (result.includes(bonus)) {
-      bonus = (Math.floor(Math.random() * 100) % 45) + 1;
+    const paper = getPaper()
+    const year = getYearLottery()
+
+    const container: HTMLElement | null = document.querySelector(".container");
+    const div: HTMLElement = document.createElement("div")
+    for (let i = 0; i < paper.length; i++) {
+      let li: HTMLElement = document.createElement("li");
+      li.textContent = `${paper[i].toString()}`;
+      div?.appendChild(li);
     }
-
-    result.sort((a, b) => a - b);
-    let li: HTMLElement = document.createElement("li");
-    li.textContent = `${result.toString()} + ${bonus}`;
-    let div: HTMLElement | null = document.querySelector(".container");
-    div?.appendChild(li);
-    disableBtn(false)
-    // btn?.removeAttribute('disabled')
-  }
-
-  const disableBtn = (status: Boolean) => {
-    const btn = document.getElementById('btn')
-    if (status) btn?.setAttribute('disabled', 'disabled')
-    else btn?.removeAttribute('disabled')
+    if (container?.children) container.textContent = ''
+    container?.appendChild(div)
   }
 
   return (
@@ -81,12 +60,13 @@ function App() {
           id="btn"
           onClick={createLotto}
         >
-          !!
+          추첨!!
         </button>
 
         <div className="container"></div>
-      </header>
-    </div>
+        <div className="yearContainer" >hello world</div>
+      </header >
+    </div >
   );
 }
 
